9d98261af83bc57aa94786e6f2ac993d1c69e6d7
AKavaa <alex.kavaleuskiy33@gmail.com>
2026-01-07 16:14:56 +0200

    add unique pointer and modified Device Manager class

--- DeviceManager.h
+++ DeviceManager.h
@@ -1,6 +1,8 @@
 #ifndef DEVICEMANAGER_H
 #define DEVICEMANAGER_H
 
+#include "Projector.H"
+
 #include <memory>
 #include <vector>
 #include "Device.H"
@@ -14,14 +16,26 @@ private:
     // stores all the devices using polymorphic container
     // this allows different device type to be stored a unique pointer
     // using the unique pointer allows automatic memory handling and ownership
-    std::vector<std::unique_ptr<Device>> devices;
+    vector<unique_ptr<Device>> devices;
 
 public:
     // calling this in main and the program will be running
-    void RunProgram();
+    void RunProgram()
+    {
+        // Creating default objects and adding them into the device vector using a unique pointer
+        // Unique pointers automatically manage memory and push_back adds the devices into the vector
+        devices.push_back(make_unique<Projector>("HDMI", 25, 2, "ProjectorCool", "SONY"));
+    }
 
     // will be showing the menu for the Smart Campus
-    void ShowMenu();
+    void ShowMenu()
+    {
+        cout << " ====== All the available devices ======" << endl;
+        for (int i = 0; i < devices.size(); i++)
+        {
+            cout << &devices[i];
+        }
+    }
 
     // user will be able to input and choose what to do inside the program
     void ChoiceHandling();
--- main.c++
+++ main.c++
@@ -5,21 +5,27 @@
 #include "Projector.H"
 #include "AirConditioning.H"
 #include "RoomLighting.H"
-#include "DoorLock.H"
+#include "DeviceManager.h"
 using namespace std;
 
 int main()
 {
 
-    int choice;
+    // int choice;
 
-    cout << "\n===== Start Smart Campus System =====" << endl;
-    cout << "1. Security Camera" << endl;
-    cout << "2. Air Condition" << endl;
-    cout << "3. Projector" << endl;
-    cout << "4. Room Lightning" << endl;
-    cout << "5. Door Lock" << endl;
-    cout << "0. Exit System" << endl;
-    cout << "Select Device :";
-    cin >> choice;
+    // cout << "\n===== Start Smart Campus System =====" << endl;
+    // cout << "1. Security Camera" << endl;
+    // cout << "2. Air Condition" << endl;
+    // cout << "3. Projector" << endl;
+    // cout << "4. Room Lightning" << endl;
+    // cout << "5. Door Lock" << endl;
+    // cout << "0. Exit System" << endl;
+    // cout << "Select Device :";
+    // cin >> choice;
+
+    DeviceManager manager;
+
+    manager.RunProgram();
+
+    manager.ShowMenu();
 }
